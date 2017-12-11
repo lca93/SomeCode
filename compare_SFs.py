e1c007ba7abed72368ea17dd957a8f621b66b802
Luca Guzzi <luca.guzzi@cern.ch>
2017-12-11 17:11:52 +0100

    modified function grah to histo added function histo to graph added diff. of sfs in compare

--- compare_SFs.py
+++ compare_SFs.py
@@ -8,6 +8,7 @@ import numpy as np
 import os, sys
 from cfg.cfg_getSFs     import varList
 from itertools import product
+from libs.getSFs_libs   import GraphToHisto, HistoToGraph
 import math
 
 ##set up root
@@ -61,7 +62,7 @@ def compare1D (key1, key2, varName, bins, outFile, etaRange = ""):
     multiG2.Add(SFgraph2)
 
     multiG1.SetTitle("SFs comparison  %s %s; %s; efficiency" % (muonID, etaRange, varName))
-    multiG2.SetTitle("SFs comparison  %s %s; %s; SF"         % (muonID, etaRange, varName))
+    multiG2.SetTitle(";%s; SF"                               % varName)
     ## set the graphs
     DAgraph1.SetMarkerStyle(22)
     MCgraph1.SetMarkerStyle(20)
@@ -88,26 +89,53 @@ def compare1D (key1, key2, varName, bins, outFile, etaRange = ""):
     SFgraph2.SetLineColor(8)
 
     SFgraph2.SetFillColor(8)
+    SFgraph1.SetFillStyle(3004)
+    SFgraph2.SetFillStyle(3004)
+
 
     ## auxiliary TH1
     supportEff = ROOT.TH1F("suppE", "", len(bins)-1, bins)
     supportRat = ROOT.TH1F("suppR", "", len(bins)-1, bins)
+    supportDif = ROOT.TH1F("suppD", "", len(bins)-1, bins)
+
+    ## SFs difference
+    aux1 = GraphToHisto(SFgraph1, bins)
+    aux2 = GraphToHisto(SFgraph2, bins)
+
+    aux1.Add( aux2, -1)
+    for i in range(aux1.GetSize()-1):
+        aux1.SetBinContent( i+1, 
+                            abs( aux1.GetBinContent(i+1) )
+            )
+
+    diffG = HistoToGraph(aux1)
+    diffG.SetMarkerStyle(20)
+    diffG.SetFillStyle(3004)
+
+    multiG3 = ROOT.TMultiGraph()
+    multiG3.Add(diffG)
+    multiG3.SetTitle("; %s; SFs difference" % varName)
 
     ##set the canvas
-    outCan = ROOT.TCanvas("outCan", "SFs comparison %s %s  -  %s muonID" % (varName, etaRange, muonID))
+    outCan = ROOT.TCanvas("outCan", "SFs comparison %s %s  -  %s muonID" % (varName, etaRange, muonID), 700, 1000)
     outCan.Divide(1, 3)
-    outCan.GetListOfPrimitives()[0].SetPad('effPad%s'   % varName, 'effPad'  , 0., 0.30, 1., 1., 0, 0)
-    outCan.GetListOfPrimitives()[1].SetPad('ratioPad%s' % varName, 'ratioPad', 0., 0.32, 1., .0, 0, 0)
+    outCan.GetListOfPrimitives()[0].SetPad('effPad%s'   % varName, 'effPad'  , 0., 0.50, 1., 1., 0, 0)
+    outCan.GetListOfPrimitives()[1].SetPad('ratioPad%s' % varName, 'ratioPad', 0., 0.52, 1., .2, 0, 0)
+    outCan.GetListOfPrimitives()[2].SetPad('diffPad%s'  % varName, 'diffPad' , 0., 0.22, 1., .0, 0, 0)
     outCan.GetListOfPrimitives()[0].SetLogx(logx)
     outCan.GetListOfPrimitives()[1].SetLogx(logx)
+    outCan.GetListOfPrimitives()[2].SetLogx(logx)
 
     outCan.GetListOfPrimitives()[0].SetGridy(True)
     outCan.GetListOfPrimitives()[1].SetGridy(True)
+    outCan.GetListOfPrimitives()[2].SetGridy(True)
     outCan.GetListOfPrimitives()[0].SetGridx(True)
     outCan.GetListOfPrimitives()[1].SetGridx(True)
+    outCan.GetListOfPrimitives()[2].SetGridx(True)
 
     outCan.GetListOfPrimitives()[0].SetBottomMargin(0.2)
     outCan.GetListOfPrimitives()[1].SetBottomMargin(0.2)
+    outCan.GetListOfPrimitives()[2].SetBottomMargin(0.2)
     
     ## legend
     leg = ROOT.TLegend(0.7, 0.5, 0.85, 0.25)
@@ -119,7 +147,6 @@ def compare1D (key1, key2, varName, bins, outFile, etaRange = ""):
     leg2 = ROOT.TLegend(0.7, 0.5, 0.85, 0.25)
     leg2.AddEntry(SFgraph1, "BCDEF SFs", "lp")
     leg2.AddEntry(SFgraph2, "GH SFs", "lp")
-
 
     ## print on same canvas
     outCan.cd(1)
@@ -134,6 +161,12 @@ def compare1D (key1, key2, varName, bins, outFile, etaRange = ""):
     leg2.Draw("same")
     multiG2.GetYaxis().SetRangeUser(0.5, 1.2)
     multiG2.GetXaxis().SetLimits(bins[0]-0.2*abs(bins[0]), 1.1*bins[-1])
+
+    outCan.cd(3)
+    supportDif.Draw()
+    multiG3.Draw("same PLE3")
+    multiG3.GetYaxis().SetRangeUser( 0, min(1.1*ROOT.TMath.MaxElement(diffG.GetN(), diffG.GetY()), 0.2))
+    multiG3.GetXaxis().SetLimits(bins[0]-0.2*abs(bins[0]), 1.1*bins[-1])
 
     outCan.Update()
     outCan.Write()
