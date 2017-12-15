033b2714b9682964dc6b9208eab0856a81cf3d31
Luca Guzzi <luca.guzzi@cern.ch>
2017-12-15 10:19:38 +0100

    modified eff_ds to include 2D effs 	1D working 	2D not working

--- eff_from_ds.py
+++ eff_from_ds.py
@@ -5,12 +5,12 @@ import json
 from collections import OrderedDict
 
 ROOT.gStyle.SetOptFit(1111)
-ROOT.gStyle.SetOptStat(0)
-#ROOT.gROOT.SetBatch(ROOT.kTRUE)
+ROOT.gStyle.SetOptStat(1000000001)
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
 ROOT.TH1.SetDefaultSumw2()
 ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = kFatal;")
 
-## return the gaus integral
+## return the gaus integral (NOTE needs bin width)
 def GetGausIntegral(norm, sigma, errNorm, errSigma):
     area = norm*sigma*math.sqrt(2.*math.pi)
     error= math.sqrt(2.*math.pi) * (norm*errSigma + sigma*errNorm)
@@ -35,10 +35,10 @@ def fitFunc (x, par):
 def BinRange(ind, bins, var):
     return 'abs(ds_%s) >= %s && abs(ds_%s) <= %s' % (var, bins[ind], var, bins[ind+1])
 
-def getBinRange (i, bins):
+def getBinRange (i, bins, sep = ','):
     down =  str(bins[i])
     up   =  str(bins[i+1])
-    return down+","+up
+    return down+sep+up
 
 ## some objects
 inFile    = ROOT.TFile.Open('/afs/cern.ch/work/l/lguzzi/samples/ds_onia2016.root')
@@ -53,23 +53,24 @@ outJson   = open('eff_from_ds.json', 'w')
 ptBins  = np.array( [8, 15 , 30, 1000])
 etaBins = np.array( [0, 0.7, 1.5])
 varList = [ ('pt' , ptBins ),
-              ('eta', etaBins)
+            ('eta', etaBins)
 ]
+
+## efficiencies graphs
+ptGraph   = ROOT.TGraphErrors()
+etaGraph  = ROOT.TGraphErrors()
+effGraphs = [ptGraph, etaGraph]
 
 ## event selection
 den = 'ds_hasphi & mu1_muonid_soft & mu2_muonid_soft & sv_prob>0.1 & sv_ls>2 & sv_cos>0.999 & hlt_dimuon0_phi_barrel & pi_pt>1.2 & ds_pt>8'
 num = '%s & hlt_doublemu3_trk_tau3mu' % den
 
-## get pt efficiencies
-for vv in varList:
-    outFile.cd()
-    outFile.mkdir(vv[0])
-    outFile.cd(vv[0])
+## 2D eff
+eff_2D = ROOT.TH2F('2Deff', '', len(ptBins)-1, 0, len(ptBins)-1, len(etaBins)-1, 0, len(etaBins)-1)
 
-    jsonStruc[vv[0]] = OrderedDict()
-
-
-    for i in range( len(vv[1])-1):
+def getEff( varName, bins):
+    jsonOut = OrderedDict()
+    for i in range( len(bins)-1):
         ## fitfunctions
         fitFuncN = ROOT.TF1('fitFnum', fitFunc, 1.8, 2.02, 8)
         fitFuncD = ROOT.TF1('fitFden', fitFunc, 1.8, 2.10, 8)
@@ -91,7 +92,7 @@ for vv in varList:
         fitFuncN.SetParameter(5, 0.01 ) ; fitFuncN.SetParLimits(5, 0, 0.07)     ; fitFuncN.SetParName(5, '#sigma_{2}') ## sigm gaus2
         fitFuncN.SetParameter(6, 500. ) ; fitFuncN.SetParLimits(6, 0, 2000)     ; fitFuncN.SetParName(6, 'q')          ## norm pol1
         fitFuncN.SetParameter(7, -1000) ; fitFuncN.SetParLimits(7, -5000, 5000) ; fitFuncN.SetParName(7, 'm')          ## ang. pol1
-       
+
         ##get the bin range
         binR = BinRange(i, vv[1], vv[0])
         
@@ -99,8 +100,8 @@ for vv in varList:
         tree.Draw("ds_mass>>histoN(40, 1.8, 2.1)", '%s & %s' % (num, binR))
         tree.Draw("ds_mass>>histoD(40, 1.8, 2.1)", '%s & %s' % (den, binR))
         
-        histoN = ROOT.gDirectory.Get('histoN') ; histoN.SetName('%s_bin%s NUM' % (vv[0], i))
-        histoD = ROOT.gDirectory.Get('histoD') ; histoN.SetName('%s_bin%s DEN' % (vv[0], i))
+        histoN = ROOT.gDirectory.Get('histoN') ; histoN.SetName('%s_bin%s NUM' % (varName, i))
+        histoD = ROOT.gDirectory.Get('histoD') ; histoD.SetName('%s_bin%s DEN' % (varName, i))
 
         ## fit the histos
         histoN.Fit(fitFuncN, "RIM")
@@ -124,30 +125,22 @@ for vv in varList:
         eff = intN[0]/intD[0]
         err = math.sqrt( ((1./intD[0])**2) * intN[1] + ((intN[0]/intD[0]**2)**2) * intD[1])
 
-        ## write to root
-        histoN.Write()
-        histoD.Write()
+         ## results
+        jsonOut[getBinRange(i, bins)] = OrderedDict()
+        jsonOut[getBinRange(i, bins)]['value'] = eff
+        jsonOut[getBinRange(i, bins)]['error'] = err
 
-        ## results
-        jsonStruc[vv[0]][getBinRange(i, vv[1])] = OrderedDict()
+    return jsonOut
 
-        jsonStruc[vv[0]][getBinRange(i, vv[1])]['NUM FIT'] = OrderedDict()
-        jsonStruc[vv[0]][getBinRange(i, vv[1])]['DEN FIT'] = OrderedDict()
+for vv in varList:
+    jsonStruc[vv[0]] = OrderedDict()
+    if not isinstance(vv[1], tuple): 
+        jsonStruc[vv[0]] = getEff( vv[0], vv[1])
+    else:
+        var1 = vv[0].split('_')[1]
+        var2 = vv[0].split('_')[0]
+        for j in range( len(vv[1][1])-1):
+            jsonStruc[vv[0]][getBinRange(j, vv[1][1])] = OrderedDict()
+            jsonStruc[vv[0]][getBinRange(j, vv[1][1])] = getEff('%s_bin%s__%s_bin%s' % (var1, j, var2), vv[1][0])
 
-        for pp in range( fitFuncN.GetNpar()):
-            jsonStruc[vv[0]][getBinRange(i, vv[1])]['NUM FIT'][fitFuncN.GetParName(pp)] = OrderedDict()
-            jsonStruc[vv[0]][getBinRange(i, vv[1])]['NUM FIT'][fitFuncN.GetParName(pp)]['value'] = fitFuncN.GetParameter(pp)
-            jsonStruc[vv[0]][getBinRange(i, vv[1])]['NUM FIT'][fitFuncN.GetParName(pp)]['error'] = fitFuncN.GetParError(pp)
-
-        for pp in range( fitFuncD.GetNpar()):
-            jsonStruc[vv[0]][getBinRange(i, vv[1])]['DEN FIT'][fitFuncD.GetParName(pp)] = OrderedDict()
-            jsonStruc[vv[0]][getBinRange(i, vv[1])]['DEN FIT'][fitFuncD.GetParName(pp)]['value'] = fitFuncD.GetParameter(pp)
-            jsonStruc[vv[0]][getBinRange(i, vv[1])]['DEN FIT'][fitFuncD.GetParName(pp)]['error'] = fitFuncD.GetParError(pp)
-
-        jsonStruc[vv[0]][getBinRange(i, vv[1])]['EFFICIENCY'] = OrderedDict()
-        jsonStruc[vv[0]][getBinRange(i, vv[1])]['EFFICIENCY']['value'] = eff
-        jsonStruc[vv[0]][getBinRange(i, vv[1])]['EFFICIENCY']['error'] = err
-
-
-outJson.write( json.dumps(jsonStruc, indent=4, sort_keys=False))
-outJson.close()
+outJson.write(json.dumps(jsonStruc, indent=4, sort_keys=False))
